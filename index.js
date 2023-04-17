50a1b10273d2567369751cff4f754b6ef6c48303
Sinman0119 <maxim.cottyn@gmail.com>
2023-04-17 12:00:31 +0200

    Change to command handeling

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-for(const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if('data' in command && 'execute' in command){
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`[WARNING] De command op locatie ${filePath} mist de nodige data of execute waarden`)
+for(const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file in commandFiles){
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command){
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] De command op locatie ${filePath} mist de nodige data of execute waarden`)
+        }
     }
 }
 
