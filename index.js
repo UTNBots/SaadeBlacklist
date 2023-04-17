ed2e7cb8f9589a2446b494df210e061cb17402b6
Sinman0119 <maxim.cottyn@gmail.com>
2023-04-17 11:29:15 +0200

    Kick on join

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {Client, Events, GatewayIntentBits} = require('discord.js');
-const {token} = require('./config.json');
+const {token,DBHost,DBUser,DBPassword,DBDatabase} = require('./config.json');
+const mysql = require('mysql2');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMembers]});
 
@@ -8,7 +9,28 @@ client.once(Events.ClientReady, c => {
 });
 
 client.on("guildMemberAdd", member => {
-    console.log(member)
-})
+    var con = mysql.createConnection({
+        connectionLimit: 10,
+        host: DBHost,
+        user: DBUser,
+        password: DBPassword,
+        database: DBDatabase,
+        insecureAuth: true
+    });
+    con.connect(function(err){
+        if(err){
+            return console.error('Error ' + err.message)
+        };
+    });
+    con.query(`SELECT * FROM Members WHERE DiscordID = ${member.id}`, (err,rows) => {
+        if (err) throw err;
+        if(rows.length > 0){
+            member.kick("SaadeBlacklist")
+        };
+    });
+    con.end(function(err){
+        if(err) throw err
+    });
+});
 
 client.login(token);
