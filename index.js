82e1c6b50675dd8717d24b93a23cd511cd12614e
Sinman0119 <maxim.cottyn@gmail.com>
2023-04-17 11:01:09 +0200

    first bot startup

---
+++ index.js
@@ -0,0 +1,10 @@
+const {Client, Events, GatewayIntentBits} = require('discord.js');
+const {token} = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMembers]});
+
+client.once(Events.ClientReady, c => {
+    console.log("Bot is opgestart");
+});
+
+client.login(token);
